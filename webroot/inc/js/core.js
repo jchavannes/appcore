858568faad819f5eda1b166e7ae1675d9282f5cc
jchavannes <jason.chavannes@gmail.com>
2012-09-29 18:52:35 -0700

    Added popup messages

--- webroot/inc/js/core.js
+++ webroot/inc/js/core.js
@@ -36,8 +36,51 @@ var CommentForm = new (function() {
 });
 var PopupMessage = new(function() {
 	this.init = function() {
-		var html = "<div class='popup'><div class='message'><h4>Title</h4>message</div></div>";
+		if($('#body_wrapper').length == 0) {
+			$('body').append("<div id='body_wrapper'></div>");
+			$('body').children(':not(#body_wrapper)').each(function() {
+				$(this).appendTo($('#body_wrapper'));
+			});
+		}
+		var html = "<div class='popup'><div class='box'><h3></h3><div class='message'></div></div></div>";
 		$('body').append(html);
 	}
+	this.show = function(message, title) {
+		if(typeof message != 'string') {return false;}
+		if(typeof title != 'string') {title = 'Message'}
+		$('.popup h3').html(title);
+		$('.popup .message').html(message);
+		$('.popup').addClass('active');
+		PopupMessage.resize();
+	}
+	this.close = function() {
+		$('.popup').removeClass('active');
+	}
+	this.resize = function() {
+		var winHeight = $(window).height();
+		$('.popup.active').each(function() {
+			var box = $(this).find('.box');
+			var body = $('body');
+			if(box.height() > winHeight) {
+				if(!body.hasClass('oversizePopup')) {
+					var ele = $('#body_wrapper *:eq(0)');
+					var scrollTop = $(document).scrollTop();
+					var marginTop = ele.css('margin-top');
+					ele.data({'margin-top': marginTop, 'scroll-top': scrollTop}).css({'margin-top': -scrollTop});
+				}
+				$('body').addClass('oversizePopup');
+				box.css({'margin-top':''});
+			} else {
+				if(body.hasClass('oversizePopup')) {
+					var ele = $('#body_wrapper *:eq(0)');
+					ele.css({'margin-top': ele.data('margin-top')});
+					$('body').removeClass('oversizePopup');
+					$(document).scrollTop(ele.data('scroll-top'));
+				}
+				box.css({'margin-top':-parseInt(box.height())/2});
+			}
+		});
+	}
 	$('document').ready(this.init);
+	$(window).resize(this.resize);
 });
