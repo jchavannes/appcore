30a962d5293ed6ea4d6c0c402408e5932cb8fc8d
noobsonly <jason.chavannes@gmail.com>
2012-12-31 17:30:53 -0800

    Refactored loader a little

--- webroot/app/plugins/Loader.php
+++ webroot/app/plugins/Loader.php
@@ -6,39 +6,46 @@ class IndexController {
 
 		Session::load();
 
-		$request = array();
-
-		global $route;
+		// Used for creating short urls
 		$reroutes = array(
 			"test" => array("admin", "comments"),
 		);
 
+		// Error page is default until we find a valid action
+		$route = array(
+			"controller" => "HomeController",
+			"action" => "errorAction"
+		);
+
+		// Get request query info
+		$request = array("url" => "", "parts" => array());
 		if(isset($_GET['q']) && !empty($_GET['q'])) {
 			$request['url'] = $_GET['q'];
 			$request['parts'] = explode("/", $request['url']);
 		} else {
-			$request['url'] = "home";
-			$request['parts'] = array("home");
+			// If no request, this is the homepage
+			$route['action'] = "defaultAction";
 		}
 
-		if(isset($reroutes[$request['parts'][0]])) {
+		// Check for potential re-route
+		if(isset($request['parts'][0]) && isset($reroutes[$request['parts'][0]])) {
 			$request['parts'][1] = $reroutes[$request['parts'][0]][1];
 			$request['parts'][0] = $reroutes[$request['parts'][0]][0];
 		}
 
-		$route = array(
-			"controller" => "HomeController",
-			"action" => "defaultAction"
-		);
-
-		if (isset($request['parts'][0])) {
+		// Find correct controller
+		if (!empty($request['parts'][0])) {
 			$controller = ucfirst($request['parts'][0])."Controller";
 			if (class_exists($controller)) {
 				$route['controller'] = $controller;
+				// We found a controller, set default action
+				$route['action'] = "defaultAction";
 			}
 		}
 
-		if (isset($request['parts'][1])) {
+		if (!empty($request['parts'][1])) {
+			// Request specifies an action, set to error until we find one
+			$route['action'] = "errorAction";
 			$action = $request['parts'][1]."Action";
 			if (method_exists($route['controller'], $action)) {
 				$route['action'] = $action;
